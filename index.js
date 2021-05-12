fd173e768fdf598859e7d0d46581c7f8a9750a94
chandresh R <chandresh.r@comviva.com>
2021-05-12 18:08:59 +0530

    initial

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Email notification sent once the Vaccine is available
+'use strict'
+
+const fs = require('fs')
+
+let rawdata = fs.readFileSync('data.json')
+let student = JSON.parse(rawdata)
+console.log(JSON.parse(student))
